b62a37d1369161eacffc25b32c90d554ee6b7314
Lawrence Manning <lawrence@aslak.net>
2023-10-06 13:40:49 +0100

    Added a parser for the local bus timetables
    Totally useless to anyone else, but useful to me. It pokes the realtime
    bus information into MQTT, which will be picked out by the display.
    
    To save load on the target server the request is only made if the
    display is on.
    
    This service will eventually be wrapped in some kind of container and
    will run on the same host as the Home Assistant and MQTT broker.

---
+++ tools/bluestar-parser/bluestar-parser.go
@@ -0,0 +1,133 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"regexp"
+	"strconv"
+	"strings"
+	"time"
+	"unicode"
+
+	"github.com/antchfx/htmlquery"
+	mqtt "github.com/eclipse/paho.mqtt.golang"
+)
+
+func removeSpace(s string) string {
+	rr := make([]rune, 0, len(s))
+	for _, r := range s {
+		if !unicode.IsSpace(r) {
+			rr = append(rr, r)
+		}
+	}
+	return string(rr)
+}
+
+type query struct {
+	url     string
+	towards string
+}
+
+type journey struct {
+	Towards           string
+	DeparturesSummary string
+}
+
+var f mqtt.MessageHandler = func(client mqtt.Client, msg mqtt.Message) {
+	// panic("oops")
+	fmt.Printf("TOPIC: %s\n", msg.Topic())
+	fmt.Printf("MSG: %s\n", msg.Payload())
+	brightness, _ = strconv.Atoi(string(msg.Payload()))
+}
+
+var brightness int = 0
+
+func downloadURL(url string) (string, error) {
+	cmd := exec.Command("curl", "-s", url)
+	var stdout, stderr bytes.Buffer
+	cmd.Stdout = &stdout
+	cmd.Stderr = &stderr
+	err := cmd.Run()
+	if err != nil {
+		return "", err
+	}
+	fmt.Println(stderr.String())
+	return stdout.String(), nil
+}
+
+func main() {
+	mqtt.DEBUG = log.New(os.Stdout, "", 0)
+	mqtt.ERROR = log.New(os.Stdout, "", 0)
+	opts := mqtt.NewClientOptions().AddBroker("tcp://10.52.0.2:1883").SetClientID("bluestar-parser")
+	opts.SetKeepAlive(2 * time.Second)
+	opts.SetDefaultPublishHandler(f)
+	opts.SetPingTimeout(1 * time.Second)
+	opts.SetUsername("mqttuser")
+	opts.SetPassword("mqttpassword")
+
+	c := mqtt.NewClient(opts)
+	if token := c.Connect(); token.Wait() && token.Error() != nil {
+		panic(token.Error())
+	}
+	if token := c.Subscribe("matrix-display/brightness", 1, nil); token.Wait() && token.Error() != nil {
+		panic(token.Error())
+	}
+
+	var extractionRe = regexp.MustCompile(`Destination - (.*?)\. Arrival time - (.*?)\.`)
+
+	for {
+		var queries = []query{
+			{
+				url:     "https://www.bluestarbus.co.uk/stops/1900HA080331",
+				towards: "Hythe",
+			},
+			{
+				url:     "https://www.bluestarbus.co.uk/stops/1900HA080333",
+				towards: "Southampton",
+			},
+		}
+
+		var journies []journey
+
+		for _, q := range queries {
+			if brightness > 0 {
+				text, err := downloadURL(q.url)
+				if err != nil {
+					panic(err)
+				}
+				doc, err := htmlquery.Parse(strings.NewReader(text))
+				if err != nil {
+					panic(err)
+				}
+				// Find all news item.
+				list := htmlquery.Find(doc, "/html/body/div/div/div/ol/li/a/p")
+				var departures []string
+
+				for i, n := range list {
+					m := extractionRe.FindStringSubmatch(htmlquery.InnerText(n))
+					if len(m) > 0 && i < 3 {
+						departures = append(departures, m[2])
+					}
+				}
+				journies = append(journies, journey{
+					Towards:           q.towards,
+					DeparturesSummary: strings.Join(departures, " "),
+				})
+			} else {
+				journies = append(journies, journey{
+					Towards:           q.towards,
+					DeparturesSummary: "Unknown",
+				})
+			}
+		}
+		b, _ := json.Marshal(journies)
+		token := c.Publish("bluestar-parser/journies", 0, true, b)
+		token.Wait()
+
+		time.Sleep(150 * time.Second)
+	}
+}
---
+++ tools/bluestar-parser/go.mod
@@ -0,0 +1,15 @@
+module aslak.net/bluestar-parser
+
+go 1.19
+
+require github.com/antchfx/htmlquery v1.3.0
+
+require (
+	github.com/antchfx/xpath v1.2.3 // indirect
+	github.com/eclipse/paho.mqtt.golang v1.4.3 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
+	github.com/gorilla/websocket v1.5.0 // indirect
+	golang.org/x/net v0.8.0 // indirect
+	golang.org/x/sync v0.1.0 // indirect
+	golang.org/x/text v0.8.0 // indirect
+)
