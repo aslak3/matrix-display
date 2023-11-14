2c23b9460abd09fc6629a9e716fddf3ad4b1d40a
Lawrence Manning <lawrence@aslak.net>
2023-11-14 18:54:14 +0000

    Initial SPI bitmap output

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 
 #include <pico/stdlib.h>
 #include <hardware/gpio.h>
-#include <hardware/pio.h>
+#include <hardware/spi.h>
 #include <pico/cyw43_arch.h>
 
 #include <FreeRTOS.h>
@@ -30,28 +30,6 @@ void rtc_task(void *dummy);
 void vApplicationTickHook(void);
 
 extern void mqtt_task(void *dummy);
-
-/* WIRING
- *
- * R1   GP2
- * B1   GP4
- * R2   GP5
- * B2   GP7
- * A    GP8
- * C    GP10
- * CLK  GP13
- * OE   GP15
- *
- * G1   GP3
- * G2   GP6
- * E    GND
- * B    GP9
- * D    GP11
- * LAT  GP14
- *
- * 0b00000000 00011111 11111111 00000000
- * 0x001fff00
-*/
 
 QueueHandle_t animate_queue;
 QueueHandle_t rtc_queue;
@@ -172,38 +150,22 @@ void matrix_task(void *dummy)
     printf("%s: core%u\n", pcTaskGetName(NULL), get_core_num());
 #endif
 
-    PIO pio = pio0;
-    uint sm_data = 0;
-    uint sm_row = 1;
-
-    uint data_prog_offs = pio_add_program(pio, &hub75_data_rgb888_program);
-    uint row_prog_offs = pio_add_program(pio, &hub75_row_program);
-    hub75_data_rgb888_program_init(pio, sm_data, data_prog_offs, DATA_BASE_PIN, CLK_PIN);
-    hub75_row_program_init(pio, sm_row, row_prog_offs, ROWSEL_BASE_PIN, ROWSEL_N_PINS, STROBE_PIN);
+    spi_init(spi_default, 1 * 1000 * 1000);
+    gpio_set_function(PICO_DEFAULT_SPI_RX_PIN, GPIO_FUNC_SPI);
+    gpio_set_function(PICO_DEFAULT_SPI_SCK_PIN, GPIO_FUNC_SPI);
+    gpio_set_function(PICO_DEFAULT_SPI_TX_PIN, GPIO_FUNC_SPI);
+    gpio_init(PICO_DEFAULT_SPI_CSN_PIN);
+    gpio_set_dir(PICO_DEFAULT_SPI_CSN_PIN, true);
 
     static fb_t output_fb;
 
     while (1) {
         fb.atomic_fore_copy_out(&output_fb);
 
-        for (int rowsel = 0; rowsel < 16; ++rowsel) {
-            for (int bit = 0; bit < 8; ++bit) {
-                hub75_data_rgb888_set_shift(pio, sm_data, data_prog_offs, bit);
-                for (int x = 0; x < FB_WIDTH; x++) {
-                    pio_sm_put_blocking(pio, sm_data, output_fb.uint32[rowsel][x]);
-                    pio_sm_put_blocking(pio, sm_data, output_fb.uint32[rowsel + 16][x]);
-                }
-                // Dummy pixel per lane
-                pio_sm_put_blocking(pio, sm_data, 0);
-                pio_sm_put_blocking(pio, sm_data, 0);
-                // SM is finished when it stalls on empty TX FIFO
-                hub75_wait_tx_stall(pio, sm_data);
-                // Also check that previous OEn pulse is finished, else things can get out of sequence
-                hub75_wait_tx_stall(pio, sm_row);
-
-                // Latch row data, pulse output enable for new row.
-                pio_sm_put_blocking(pio, sm_row, rowsel | (100u * (1u << bit) << 5));
-            }
+        gpio_put(PICO_DEFAULT_SPI_CSN_PIN, false);
+        for (int rowsel = 0; rowsel < FB_HEIGHT; rowsel++) {
+            spi_write_blocking(spi_default, (uint8_t *) &output_fb.uint32[rowsel][0], FB_WIDTH * sizeof(uint32_t));
         }
+        gpio_put(PICO_DEFAULT_SPI_CSN_PIN, true);
     }
 }
