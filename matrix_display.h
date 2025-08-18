ab72da86ac2724b8da125152bac47919f5afb400
Lawrence Manning <lawrence@aslak.net>
2025-08-18 23:37:54 +0100

    Add filename and line number to debug macro
    Using modern GCC trickery the leading path is removed from __FILE__.

--- matrix_display.h
+++ matrix_display.h
@@ -1,7 +1,12 @@
 #define DEBUG_MSGS
 
+// two macros ensures any macro passed will
+// be expanded before being stringified
+#define STRINGIZE_DETAIL(x) #x
+#define STRINGIZE(x) STRINGIZE_DETAIL(x)
+
 #ifdef DEBUG_MSGS
-#define DEBUG_printf(...) printf(__VA_ARGS__)
+#define DEBUG_printf(...) printf(__FILE__ ":" STRINGIZE(__LINE__) " " __VA_ARGS__)
 #else
 #define DEBUG_printf(...)
 #endif
